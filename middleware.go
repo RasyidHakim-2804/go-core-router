ddfe5d3e379f122eda6f945fa4e25b793379215b
Ahmad Rasyid Hakim S <ragamgit78@gmail.com>
2025-03-06 10:08:30 +0800

    add func generateSliceMiddlewareAndStatus

--- middleware.go
+++ middleware.go
@@ -8,3 +8,26 @@ import (
 type Middleware interface {
 	Next(w http.ResponseWriter, r *http.Request) bool
 }
+
+type MiddlewareAndStatus struct {
+	Middleware Middleware
+	status     bool
+}
+
+func generateSliceMiddlewareAndStatus(slice []MiddlewareAndStatus, middleware Middleware, status bool) []MiddlewareAndStatus {
+	notExists := true
+
+	for i := range slice {
+		if slice[i].Middleware == middleware {
+			slice[i].status = status
+			notExists = false
+			break
+		}
+	}
+
+	if notExists {
+		slice = append(slice, MiddlewareAndStatus{middleware, status})
+	}
+
+	return slice
+}
