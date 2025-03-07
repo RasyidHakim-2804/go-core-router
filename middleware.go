e8084ff07ba4fcb421bc74461084fc31831d87ca
Ahmad Rasyid Hakim S <ragamgit78@gmail.com>
2025-03-07 14:14:27 +0800

    add Middleware struct

--- middleware.go
+++ middleware.go
@@ -4,17 +4,28 @@ import (
 	"net/http"
 )
 
-// Middleware defines the interface for handling middleware logic.
-type Middleware interface {
+type MiddlewareInterface interface {
 	Next(w http.ResponseWriter, r *http.Request) bool
+	After(w http.ResponseWriter, r *http.Request) bool
+}
+
+type Middleware struct {
+}
+
+func (m Middleware) Next(w http.ResponseWriter, r *http.Request) bool {
+	return true
+}
+
+func (m Middleware) After(w http.ResponseWriter, r *http.Request) bool {
+	return true
 }
 
 type MiddlewareAndStatus struct {
-	Middleware Middleware
+	Middleware MiddlewareInterface
 	status     bool
 }
 
-func generateSliceMiddlewareAndStatus(slice []MiddlewareAndStatus, middleware Middleware, status bool) []MiddlewareAndStatus {
+func generateSliceMiddlewareAndStatus(slice []MiddlewareAndStatus, middleware MiddlewareInterface, status bool) []MiddlewareAndStatus {
 	notExists := true
 
 	for i := range slice {
